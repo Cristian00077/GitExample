dfc231e7060c0a3677d07267d32b1f7841e509da
Cristian00077 <cristianviloria.14@gmail.com>
2025-03-12 18:19:15 -0500

    segundo commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,15 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package main;
 
-/**
- *
- * @author CRISTIAN
- */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!");
+        System.out.println("Hola");
     }
 }
